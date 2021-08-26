a827190a35434c0e1a5801718b951978fede916a
fernandopaz1 <ferpaz9292@gmail.com>
2021-08-26 00:11:34 -0300

    feat: funcion de numeros aleatorios

--- main.c
+++ main.c
@@ -3,9 +3,23 @@
 #include<math.h>
 #include<time.h>
 
+int rand(void);
+float aleatorio(void);
+
+
 int main(int argc,char *argv[]){
 
-    printf("Hola");
-
+    int i=0;
+    while(i<1000){
+        printf("Aleatorio  %f \n", aleatorio());
+        i++;
+    }
     return 0;
+}
+
+
+float aleatorio(void){
+	float a;
+	a=((float)rand())/((float)RAND_MAX);
+	return a;
 }
